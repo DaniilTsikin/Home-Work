95b6db33d1d6ffa56519a5b077b1b9af58206b2b
Daniil <dtsikin@mail.ru>
2022-02-06 15:14:53 +0500

    Написали программу для выяснения чётности числа

--- HW3/Program.cs
+++ HW3/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число : ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA % 2 == 0)
+{
+    Console.WriteLine("Чётное");
+}
+else
+{
+    Console.WriteLine("Нечётное");
+}
