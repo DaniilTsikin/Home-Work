a54b37c814056d1911f85d060bd5e4086c75c759
Daniil <dtsikin@mail.ru>
2022-02-06 14:10:37 +0500

    Написали программу, для нахождения большего и меньшего из двух чисел

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA = new Random().Next(1,11);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,11);
+Console.WriteLine(numberB);
+int max = numberA;
+int min = numberA;
+
+if (max <= numberB)
+{
+    max = numberB;
+}
+if (min >= numberB)
+{
+    min = numberB;
+}
+Console.WriteLine(max);
+Console.WriteLine(min);
